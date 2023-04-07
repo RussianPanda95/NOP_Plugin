1b8296ab589a59500763506775eb82f00b53b3f3
RussianPanda <91575783+RussianPanda95@users.noreply.github.com>
2023-04-07 13:29:02 -0400

    Add files via upload

---
+++ NOP_Plugin.py
@@ -0,0 +1,36 @@
+import ida_bytes
+import idautils
+import idaapi
+import idc
+
+class NopPlugin(idaapi.plugin_t):
+    flags = idaapi.PLUGIN_KEEP
+    comment = "NOP selected instructions"
+    help = ""
+    wanted_name = "NOPPlugin"
+    wanted_hotkey = "Shift+Z"
+
+    def init(self):
+        return idaapi.PLUGIN_OK
+
+    def run(self, arg):
+        # Get the selected instructions
+        selection = []
+        selstart, selend = idc.read_selection_start(), idc.read_selection_end()
+        if selstart == idc.BADADDR or selend == idc.BADADDR:
+            print("Select an instruction or a range of instructions.")
+            return
+        for head in idautils.Heads(selstart, selend):
+            selection.append(head)
+
+        # NOP the selected instructions
+        for ea in selection:
+            length = ida_bytes.get_item_size(ea)
+            nop_opcode = b"\x90" * length
+            ida_bytes.patch_bytes(ea, nop_opcode)
+
+    def term(self):
+        pass
+
+def PLUGIN_ENTRY():
+    return NopPlugin()
